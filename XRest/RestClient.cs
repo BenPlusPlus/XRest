a0076fef8901f7e0ad30b8d8f698f5ce130e21c4
Ben Baird <ben@excalsoft.net>
2020-08-13 13:16:45 -0600

    Constructor for RestClient class

--- XRest/RestClient.cs
+++ XRest/RestClient.cs
@@ -1,8 +1,28 @@
 ﻿using System;
+using System.Net.Http;
 
 namespace XRest
 {
-    public class Class1
+    public class RestClient
     {
+        private string _baseUrl;
+        private HttpClient _httpClient;
+
+        public RestClient(string BaseUrl)
+        {
+            Init(BaseUrl, new HttpClient());
+        }
+
+        public RestClient(string BaseUrl, HttpClient HttpClient)
+        {
+            Init(BaseUrl, HttpClient);
+        }
+
+        private void Init(string BaseUrl, HttpClient HttpClient)
+        {
+            _baseUrl = BaseUrl;
+            _httpClient = HttpClient;
+        }
     }
 }
+
