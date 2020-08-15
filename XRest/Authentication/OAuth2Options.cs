a1fdc3ebfe3d5417e048ba3a43557abc5f7da1af
Ben Baird <ben@excalsoft.net>
2020-08-15 17:17:49 -0600

    Early stages of OAuth2 support

---
+++ XRest/Authentication/OAuth2Options.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XRest.Authentication
+{
+    public class OAuth2Options
+    {
+        public Uri TokenUrl { get; set; }
+        public string ClientId { get; set; }
+        public string ClientSecret { get; set; }
+    }
+}
+
